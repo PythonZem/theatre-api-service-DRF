4c5c6d6e79bad880e7e0e02457ef5759a7a13427
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-03-28 00:01:53 +0200

    add validator for  TicketSerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from .models import (
     Actor,
@@ -55,6 +56,16 @@ class ReservationSerializer(serializers.ModelSerializer):
 
 class TicketSerializer(serializers.ModelSerializer):
 
+    def validate(self, attrs):
+        data = super(TicketSerializer, self).validate(attrs=attrs)
+        Ticket.validate_ticket(
+            attrs["row"],
+            attrs["seat"],
+            attrs["performance"].theatrehall,
+            ValidationError
+        )
+        return data
+
     class Meta:
         model = Ticket
-        fields = ("id", "row", "seat", "performance", "reservation")
+        fields = ("id", "row", "seat", "performance")
