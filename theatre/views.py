943dc37cd720326feffceaa58a2310f5b141e623
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-03-28 13:10:09 +0200

    Add for PlayViewSet filtering by title, actors and genres. Also add 2 serializers for list and detail

--- theatre/views.py
+++ theatre/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ModelViewSet
 
 from .models import (
@@ -16,6 +17,8 @@ from .serializers import (
     PerformanceSerializer,
     ReservationSerializer,
     TheatreHallSerializer,
+    PlayListSerializer,
+    PlayDetailSerializer,
 )
 
 
@@ -29,9 +32,44 @@ class GenreViewSet(ModelViewSet):
     serializer_class = GenreSerializer
 
 
-class PlayViewSet(ModelViewSet):
-    queryset = Play.objects.all()
+class PlayViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Play.objects.prefetch_related("actors", "genres")
     serializer_class = PlaySerializer
+
+    @staticmethod
+    def _params_to_ints(qs):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in qs.split(",")]
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if title := self.request.query_params.get("title"):
+            queryset = queryset.filter(title__icontains=title)
+
+        if actor := self.request.query_params.get("actor"):
+            actors_id = self._params_to_ints(actor)
+            queryset = queryset.filter(actor__id__in=actors_id)
+
+        if genre := self.request.query_params.get("genres"):
+            genres_id = self._params_to_ints(genre)
+            queryset = queryset.filter(genre__id__in=genres_id)
+
+        return queryset.distinct()
+
+    def get_serializer_class(self):
+        if self.action == "List":
+            return PlayListSerializer
+
+        if self.action == "retrieve":
+            return PlayDetailSerializer
+
+        return PlaySerializer
 
 
 class TheatreHallViewSet(ModelViewSet):
