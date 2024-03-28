0546582ebae12e890bba31b3da6becee3ca960ba
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-03-28 12:47:43 +0200

    add detail and list serialize for play model

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -33,6 +33,28 @@ class PlaySerializer(serializers.ModelSerializer):
         fields = ("id", "title", "description", "actors", "genres")
 
 
+class PlayListSerializer(PlaySerializer):
+    genres = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+    actors = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="full_name"
+    )
+
+    class Meta:
+        model = Play
+        fields = ("id", "title", "genres", "actors",)
+
+
+class PlayDetailSerializer(PlaySerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Play
+        fields = ("id", "title", "description", "actors", "genres",)
+
+
 class TheatreHallSerializer(serializers.ModelSerializer):
 
     class Meta:
